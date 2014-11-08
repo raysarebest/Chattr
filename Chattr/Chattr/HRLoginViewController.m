64f7265f5c9482f9088ffa9514cea584ee355043
Michael Hulet <bestraysfan@me.com>
2014-11-08 15:09:38 -0500

    Created login and sign up view controllers

---
+++ Chattr/Chattr/HRLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  HRLoginViewController.m
+//  Chattr
+//
+//  Created by Michael Hulet on 11/8/14.
+//  Copyright (c) 2014 Human Productions. All rights reserved.
+//
+
+#import "HRLoginViewController.h"
+
+@interface HRLoginViewController ()
+
+@end
+
+@implementation HRLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationController.navigationBar.hidden = YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Chattr/Chattr/HRSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  HRSignUpViewController.h
+//  Chattr
+//
+//  Created by Michael Hulet on 11/8/14.
+//  Copyright (c) 2014 Human Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HRSignUpViewController : UIViewController
+
+@end
